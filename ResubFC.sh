ffde2046a37b0ce6470de799dee14390da7a9c75
Calum Towler <calumnatowler@hotmail.com>
2018-12-03 10:01:31 +0000

    bug fix in ResubFC

--- ResubFC.sh
+++ ResubFC.sh
@@ -4,10 +4,10 @@
 
 for (( i=$1; i<=$2; i++ ))
 do
-	cd MP$1/FixedCell/
-	qsub f_MP$1.script
+	cd MP$i/FixedCell/
+	qsub f_MP$i.script
 	cd ../..
-	echo Submitted MP$1
+	echo Submitted MP$i
 done
 
 
