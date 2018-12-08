9212a11cbb0838c7b13e18ce09612a40207a7c16
Calum Towler <calumnatowler@hotmail.com>
2018-12-08 11:26:00 +0000

    initcom CatCalcProp and added to usefulfiles. all functions in useful files now tested and working

---
+++ kpoint_testing/CatCalcProp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# A script that loops through the directories, searches for the phonon frequencies in the OUTCAR and writes it to a file called PhoFreq.txt
+
+touch AllCellDims AllCellVols AllPhoFreqs
+
+for (( i=$1; i<=$2; i++ ))
+do
+        cd MP$i/
+	cat CellDim.txt >> ../AllCellDims
+	cat CellVol.txt >> ../AllCellVols
+	cd FixedCell/Dielectric
+	cat PhoFreq.txt >> ../../../AllPhoFreqs
+        echo Finished with MP$i, moving to next directory...
+        cd ../../..
+        echo Finished!
+done
+
