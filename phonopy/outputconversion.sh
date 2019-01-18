5766bcb6b6a123e1697540580675ff83dbc24dc6
Calum Towler <calumnatowler@hotmail.com>
2019-01-18 15:25:11 +0000

    added file collection functionality for phonopy

--- phonopy/outputconversion.sh
+++ phonopy/outputconversion.sh
@@ -7,5 +7,12 @@ mv qpoints.yaml BORNqpoints.yaml
 cd BORN/
 cp OUTCAR OUTCAR.born
 cd ..
-echo Done!
+echo Files have been renamed...
+
+mkdir Usefulfiles_$1_Phonopy
+mkdir Usefulfiles_$1_Phonopy/BORN
+cp BORNphonopy.yaml BORNqpoints.yaml Usefulfiles_$1_Phonopy
+cp BORN/OUTCAR.born Usefulfiles_$1_Phonopy/BORN
+echo Files have been copied...
+echo Done
 
